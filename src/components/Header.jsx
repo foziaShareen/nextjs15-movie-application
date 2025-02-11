98eb287c55ec47d4cf8dabcc18c3ddcc425c5bd7
foziaShareen <foziashareen1@gmail.com>
2025-02-11 19:43:06 +0500

    header completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import MenuItem from './MenuItem'
 import {AiFillHome} from 'react-icons/ai'
+import { AiFillInfoCircle } from "react-icons/ai";
+import Link from 'next/link'
+
+
 
 
 export default function Header() {
     return (
-        <div>
-            <div><MenuItem title="Home" address="/" Icon={AiFillHome}/></div>
-            <div></div>
+        <div className='flex justify-between items-center   max-w-6xl mx-auto:'>
+            <div className='flex  gap-4'><MenuItem title="Home" address="/" Icon={AiFillHome}/>
+            <MenuItem title="about" address="/" Icon={AiFillInfoCircle} />
+            </div>
+            <div className='flex items-center gap-2'>
+                <Link href={'/'}className='font-bold  text-2xl bg-amber-500  px-1 rounded'>IMDb</Link>
+                <span>Clone</span>
+            </div>
+            
         </div>
     )
 }

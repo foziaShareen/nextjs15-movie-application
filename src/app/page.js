7dc6543411b39bef5598cab899a1ebac2e44206d
foziaShareen <foziashareen1@gmail.com>
2025-02-12 08:40:39 +0500

    fetch movies

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import Results from '@/components/Results';
 
-export default function page() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  // const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=9c01baf6&language=en-US&page=1`,
+  );
+  const data = await res.json();
+  
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data; // Adjust this line based on the actual structure of the response
+  console.log(results);
+
   return (
     <div>
-      home
-      
+      {/* <Results results={results} /> */}
     </div>
-  )
+  );
 }
